8de49e32de1e77ee8e6d76c870cbf38d834e44f9
Kayvan <alternatiph@gmail.com>
2021-06-16 14:26:56 +0430

    bug fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 express()
   .get('/', (req, res) => res.send('works'))
   .get('/binance/get-market/:pricingMarketSymbol', async (req, res) => {
-    const url = `https://api.binance.com/api/v3/ticker/price?symbol=${req.params.pricingMarketSymbo}`
+    const url = `https://api.binance.com/api/v3/ticker/price?symbol=${req.params.pricingMarketSymbol}`
     console.log(url)
     const requestConfig = {
       // url: `https://api.binance.com/api/v3/trades?symbol=${req.params.pricingMarketSymbol}&limit=1`,
