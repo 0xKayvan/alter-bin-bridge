63945fdf7ffb77165b48de1ea6d9aa07e728e1d1
Kayvan <alternatiph@gmail.com>
2021-06-16 14:17:44 +0430

    new-apis

--- index.js
+++ index.js
@@ -6,18 +6,40 @@ express()
   .get('/', (req, res) => res.send('works'))
   .get('/binance/get-market/:pricingMarketSymbol', async (req, res) => {
     const requestConfig = {
-      url: `https://api.binance.com/api/v3/trades?symbol=${req.params.pricingMarketSymbol}&limit=1`
+      // url: `https://api.binance.com/api/v3/trades?symbol=${req.params.pricingMarketSymbol}&limit=1`,
+      url: `https://api.binance.com/api/v3/ticker/price?symbol=${pricingMarketSymbol}`
     }
     try {
       const response = await axios(requestConfig)
-      const result = response ? response.data : null
-      console.log(result)
-      if (result && Array.isArray(result) && result.length > 0) {
-        // const lastTrade = result[0]
-        return res.json(result)
-      }
+      res.json(response)
     } catch (err) {
       res.sendStatus(400)
     }
+  })
+  .get('/binance/get-fees/:pricingMarketSymbol', async (req, res) => {
+    const timestamp = Date.now()
+    const paramsObject = {'timestamp': timestamp}
+    const binanceApiKey = process.env.BINANCE_SPOT_MARKET_API_KEY
+    const binanceSecretKey = process.env.BINANCE_SPOT_MARKET_SECRET_KEY
+
+    const queryString = Object.keys(paramsObject).map((key) => {
+      return `${key}=${paramsObject[key]}`
+    }).join('&')
+    const signature = CryptoJS.HmacSHA256(queryString, binanceSecretKey).toString()
+
+    const requestConfig = {
+      url: `https://api.binance.com/sapi/v1/capital/config/getall?timestamp=${timestamp}&signature=${signature}`,
+      headers: {
+        'X-MBX-APIKEY': binanceApiKey
+      }
+    }
+
+    const response = await axios(requestConfig)
+    res.json(response)
+    // const result = response?.data
+    // if (result && Array.isArray(result) && result.length > 0) {
+      // return result
+    // }
+    // return null
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
